46d1f965614fbd8c7edd449cced7e1b336b248b0
Ignacio Narvaja <85379690+ignacionar@users.noreply.github.com>
2024-07-01 09:22:32 -0300

    Update login.service.ts

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -3,13 +3,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_dSa3hw7fhBIcCW08tgVR4FQ0NbdaxD4",
-  authDomain: "nest-firebase-d90cb.firebaseapp.com",
-  projectId: "nest-firebase-d90cb",
-  storageBucket: "nest-firebase-d90cb.appspot.com",
-  messagingSenderId: "784636965674",
-  appId: "1:784636965674:web:7cbafb7c32c8ef81906453",
-  measurementId: "G-T49T8S0H5F"
+  // ...
 };
 
 @Injectable()
